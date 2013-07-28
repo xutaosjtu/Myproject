a6cd66981d97bc595db39e83ad7daacb0e59cd5b
tao.xu <xutaosjtu@gmail.com>
2013-07-28 23:06:07 +0200

    finished data generation

--- testData_generator.R
+++ testData_generator.R
@@ -1,25 +1,6 @@
 N = 100
 
-## A~J cities
-set.seed(10)
-link.air = matrix(rbinom(N,1, 0.3), ncol = 10)
-colnames(link.air) = LETTERS[1:10]
-rownames(link.air) = LETTERS[1:10]
-tmp = mapply(cost, mean = 100,link.air)
-ticket.air  = matrix(tmp, ncol = 10)
 
-set.seed(234)
-link.train = matrix(rbinom(N,1, 0.3), ncol = 10)
-colnames(link.train) = LETTERS[1:10]
-rownames(link.train) = LETTERS[1:10]
-tmp = mapply(cost, mean=50,link.train)
-ticket.train  = matrix(tmp, ncol = 10)
-
-link.bus = matrix(rep(1, 100), ncol = 10)
-colnames(link.bus) = LETTERS[1:10]
-rownames(link.bus) = LETTERS[1:10]
-tmp = mapply(cost, mean = 30,link.bus)
-ticket.train  = matrix(tmp, ncol = 10)
 
 ## within city
 places = data.frame(name, price, time, start, end)
@@ -30,3 +11,53 @@ hotel = sapply(hotel.mean, function(x) abs(rnorm(10, mean = x, sd = 5)))
 rownames(hotel) = LETTERS[1:10]
 colnames(hotel) = letters[1:10]
 
+##Table of cities
+city = LETTERS[1:10] # ten cities
+Scene.name = sapply(city, function(x) paste(x, 1:10))
+Scene.address = mapply(function(x) paste(x, letters[1]), Scene.name)
+CITY = data.frame(city = rep(city, each = 10), Scene.name = as.vector(Scene.name), Scene.address, Scene.time = sample((1:10)/2, 100, replace=T))
+write.csv(CITY, file = "cities.csv", row.names=F)
+
+## Travel.plane
+set.seed(10)
+link.air = matrix(rbinom(100,1, 0.3), ncol = 10)
+colnames(link.air) = LETTERS[1:10]
+rownames(link.air) = LETTERS[1:10]
+tmp = mapply(cost, mean = 100,link.air)
+ticket.air  = matrix(tmp, ncol = 10)
+TRAVEL.PLANE = extract(ticket.air, city, city)
+write.csv(TRAVEL.PLANE, file="travel.plane.csv", row.names = F)
+
+## Travel.train
+set.seed(234)
+link.train = matrix(rbinom(100,1, 0.3), ncol = 10)
+colnames(link.train) = LETTERS[1:10]
+rownames(link.train) = LETTERS[1:10]
+tmp = mapply(cost, mean=50,link.train)
+ticket.train  = matrix(tmp, ncol = 10)
+TRAVEL.TRAIN = extract(ticket.train, city, city)
+write.csv(TRAVEL.TRAIN, file="travel.train.csv", row.names = F)
+
+## Travel.bus
+link.bus = matrix(rep(1, 100), ncol = 10)
+colnames(link.bus) = LETTERS[1:10]
+rownames(link.bus) = LETTERS[1:10]
+tmp = mapply(cost, mean = 30,link.bus)
+ticket.bus = matrix(tmp, ncol = 10)
+TRAVEL.BUS = extract(ticket.bus, city, city)
+write.csv(TRAVEL.BUS, file="travel.bus.csv", row.names = F)
+
+## Taxi
+TAXI = data.frame(city, start = rnorm(10, mean = 10, sd=1), Price=rnorm(10, mean = 2,sd=1))
+write.csv(TAXI, file="Travel.taxi.csv", row.names=F)
+
+##Hotel
+rst = NULL
+for(c in city){
+  num.hotels = sample(c(5:10),1)
+  hotels = paste(c, num.hotels)
+  price = rnorm(num.hotels, mean = 100, sd = 10)
+  tmp = data.frame(rep(c, num.hotels), hotels, price)
+  rst = rbind(rst,tmp)
+}
+write.csv(rst, file = "hotels.csv")
--- tools.R
+++ tools.R
@@ -1,10 +1,20 @@
 cost = function(x,...){
   if(x==1){
-    price = rnorm(50, ...)
+    price = rnorm(5, ...)
     price = abs(price)
   }
   else{
     price = NA
   }
   return(price)
+}
+
+extract = function(m, start, dest, ...){
+  index = which(!is.na(m), arr.ind=T)
+   data = data.frame(
+     City.start = rep(start[index[,1]], each = 5), 
+     City.end = rep(dest[index[,2]], each = 5),
+     Price = unlist(m[which(!is.na(m))])
+   )
+  return(data)
 }
